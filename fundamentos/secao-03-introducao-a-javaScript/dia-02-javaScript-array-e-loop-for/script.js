3a750c4aa97455ab303a287f60bd7e8eb34d667c
Thiago Freire <oreidometabolismo@gmail.com>
2023-01-29 13:45:52 -0300

    Resolvendo problemas com  For e For/of

--- fundamentos/secao-03-introducao-a-javaScript/dia-02-javaScript-array-e-loop-for/script.js
+++ fundamentos/secao-03-introducao-a-javaScript/dia-02-javaScript-array-e-loop-for/script.js
@@ -7,9 +7,46 @@ for(let indice = 0; indice < groceryList.length; indice+= 1) {
     console.log(groceryList[indice]);
 }
 
+//Utilize o for/of para imprimir os elementos da lista names com o console.log()://
 let names = ['João', 'Maria', 'Antônio', 'Margarida'];
-for (let nomes of names){
-    console.log(nomes);
+
+for(let nome of names) {
+  console.log(nome);
 }
 
-xxxxx
+/*Iremos utilizar esse array para realizar os próximos exercícios.
+
+Copiar
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+Some todos os valores contidos no array e imprima o resultado;
+
+Calcule e imprima a média aritmética dos valores contidos no array;
+
+A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+
+Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+resultado = 0;
+/*for (let number of numbers) {
+    console.log(number);
+}*/
+
+for (let soma = 0; soma < numbers.length; soma = soma + 1) {
+    resultado += numbers[soma];
+    
+}
+console.log (resultado);
+
+
